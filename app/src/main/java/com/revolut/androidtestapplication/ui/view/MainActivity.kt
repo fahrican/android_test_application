49fb568cb995de4a10d76fe2650cad8594177409
Fahri MacBook <fahri-can@web.de>
2020-07-03 16:21:56 +0200

    Move Activity to new package.

--- app/src/main/java/com/revolut/androidtestapplication/ui/view/MainActivity.kt
+++ app/src/main/java/com/revolut/androidtestapplication/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.revolut.androidtestapplication
+package com.revolut.androidtestapplication.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.revolut.androidtestapplication.R
 
 class MainActivity : AppCompatActivity() {
 
