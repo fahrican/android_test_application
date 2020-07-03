440034c555b618542d47ff63e9a21bf8ed2fe603
Fahri MacBook <fahri-can@web.de>
2020-07-04 00:05:02 +0200

    Create file for later use of all constants.

---
+++ app/src/main/java/com/revolut/androidtestapplication/internal/Constant.kt
@@ -0,0 +1,6 @@
+package com.revolut.androidtestapplication.internal
+
+
+const val BASE_URL = "https://hiring.revolut.codes/"
+const val ENDPOINT = "api/android/latest"
+const val BASE_CURRENCY = "base"
