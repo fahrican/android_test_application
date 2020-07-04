325a67d064167df7cc64567e995078563d67020f
Fahri MacBook <fahri-can@web.de>
2020-07-04 12:28:33 +0200

    Add more properties.

--- app/src/main/java/com/revolut/androidtestapplication/model/Currency.kt
+++ app/src/main/java/com/revolut/androidtestapplication/model/Currency.kt
@@ -1,3 +1,8 @@
 package com.revolut.androidtestapplication.model
 
-data class Currency(val flag: String, val currencyName: String)
+data class Currency(
+    val flag: String,
+    val shortName: String,
+    val fullName: String,
+    val rate: Double
+)
