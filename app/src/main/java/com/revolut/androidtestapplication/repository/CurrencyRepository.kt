82a6f6616e92d370cb11bd3454ac9729f44514fd
Fahri MacBook <fahri-can@web.de>
2020-07-04 20:53:15 +0200

    Finish implementing repo.

--- app/src/main/java/com/revolut/androidtestapplication/repository/CurrencyRepository.kt
+++ app/src/main/java/com/revolut/androidtestapplication/repository/CurrencyRepository.kt
@@ -1,10 +1,18 @@
 package com.revolut.androidtestapplication.repository
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.revolut.androidtestapplication.data.network.RevolutCurrencyApi
 import com.revolut.androidtestapplication.di.DaggerAppComponent
+import com.revolut.androidtestapplication.helper.CurrencyHolder
 import com.revolut.androidtestapplication.model.Currency
+import com.revolut.androidtestapplication.model.EndpointResponse
+import io.reactivex.SingleObserver
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.observers.DisposableObserver
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class CurrencyRepository {
@@ -29,6 +37,26 @@ class CurrencyRepository {
         DaggerAppComponent.create().inject(this)
     }
 
-
+    fun fetchCurrencies(): Disposable { //todo: here should go in the currency code
+        return revolutService.getEndpointResponse("EUR")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+                    _isInProgress.postValue(true)
+                    if (it != null) {
+                        val currencyHolder = CurrencyHolder()
+                        currencyHolder.insertCurrencies(it)
+                        _currencies.postValue(currencyHolder.listOfCurrencies)
+                    }
+                    _isInProgress.postValue(false)
+                },
+                {
+                    _isInProgress.postValue(true)
+                    Log.e("subscribeToObserver()", "network error: ${it.message}")
+                    _isError.postValue(true)
+                    _isInProgress.postValue(false)
+                })
+    }
 
 }
