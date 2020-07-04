292af6e8cf8af8ca93383e093dfafbdde21561f1
Fahri MacBook <fahri-can@web.de>
2020-07-04 13:23:35 +0200

    Hardcoded currency values like flag, code and name.

---
+++ app/src/main/java/com/revolut/androidtestapplication/helper/CurrencyHolder.kt
@@ -0,0 +1,261 @@
+package com.revolut.androidtestapplication.helper
+
+import com.revolut.androidtestapplication.model.Currency
+import com.revolut.androidtestapplication.model.Rates
+
+
+class CurrencyHolder {
+
+    val listOfCurrencies = ArrayList<Currency>()
+
+    fun getListOfCurrencies() {
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/aus.svg",
+                "AUD",
+                "Australian dollar",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/bgr.svg",
+                "BGN",
+                "Bulgarian lev",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/bra.svg",
+                "BRL",
+                "Brazilian real",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/can.svg",
+                "CAD",
+                "Canadian dollar",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/che.svg",
+                "CHF",
+                "Swiss franc",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/chn.svg",
+                "CNY",
+                "Chinese yuan",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/cze.svg",
+                "CZK",
+                "Czech koruna",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/dnk.svg",
+                "DKK",
+                "Danish krone",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/gbr.svg",
+                "GBP",
+                "British pound",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/hkg.svg",
+                "HKD",
+                "Hong Kong dollar",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/hrv.svg",
+                "HRK",
+                "Croatian kuna",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/hun.svg",
+                "HUF",
+                "Hungarian forint",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/idn.svg",
+                "IDR",
+                "Indonesian rupiah",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/isr.svg",
+                "ILS",
+                "Israeli new shekel",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/ind.svg",
+                "INR",
+                "Indian rupee",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/isl.svg",
+                "ISK",
+                "Icelandic króna",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/jpn.svg",
+                "JPY",
+                "Japanese yen",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/kor.svg",
+                "KRW",
+                "South Korean won",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/mex.svg",
+                "MXN",
+                "Mexican peso",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/mys.svg",
+                "MYR",
+                "Malaysian ringgit",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/nor.svg",
+                "NOK",
+                "Norwegian krone",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/nzl.svg",
+                "NZD",
+                "New Zealand dollar",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/phl.svg",
+                "PHP",
+                "Philippine peso",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/pol.svg",
+                "PLN",
+                "Polish złoty",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/rou.svg",
+                "RON",
+                "Romanian leu",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/rus.svg",
+                "RUB",
+                "Russian ruble",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/swe.svg",
+                "SEK",
+                "Swedish krona",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/sgp.svg",
+                "SGD",
+                "Singapore dollar",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/tha.svg",
+                "THB",
+                "Thai baht",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/usa.svg",
+                "USD",
+                "United States dollar",
+                0.0
+            )
+        )
+        listOfCurrencies.add(
+            Currency(
+                "https://restcountries.eu/data/zaf.svg",
+                "ZAR",
+                "South African rand",
+                0.0
+            )
+        )
+    }
+}
