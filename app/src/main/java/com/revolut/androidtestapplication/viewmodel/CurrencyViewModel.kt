77852a3edaebb9301382f6545b3cb2c8a48a9bd9
Fahri MacBook <fahri-can@web.de>
2020-07-04 21:08:10 +0200

    Implement ViewModel.

---
+++ app/src/main/java/com/revolut/androidtestapplication/viewmodel/CurrencyViewModel.kt
@@ -0,0 +1,28 @@
+package com.revolut.androidtestapplication.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.revolut.androidtestapplication.di.DaggerAppComponent
+import com.revolut.androidtestapplication.repository.CurrencyRepository
+import io.reactivex.disposables.CompositeDisposable
+import javax.inject.Inject
+
+class CurrencyViewModel : ViewModel() {
+
+
+    @Inject
+    lateinit var repository: CurrencyRepository
+
+    private val compositeDisposable by lazy { CompositeDisposable() }
+
+
+    init {
+        DaggerAppComponent.create().inject(this)
+        compositeDisposable.add(repository.fetchCurrencies())
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
+
+}
