b390fff2686dbfd8e008f6a8730a12f99d4cf459
Fahri MacBook <fahri-can@web.de>
2020-07-04 21:34:25 +0200

    Implement first logic to display data.

--- app/src/main/java/com/revolut/androidtestapplication/ui/view/MainActivity.kt
+++ app/src/main/java/com/revolut/androidtestapplication/ui/view/MainActivity.kt
@@ -2,12 +2,100 @@ package com.revolut.androidtestapplication.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DefaultItemAnimator
 import com.revolut.androidtestapplication.R
+import com.revolut.androidtestapplication.di.DaggerAppComponent
+import com.revolut.androidtestapplication.ui.adapter.CurrencyAdapter
+import com.revolut.androidtestapplication.viewmodel.CurrencyViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+
+    @Inject
+    lateinit var currencyAdapter: CurrencyAdapter
+
+    private val viewModel: CurrencyViewModel by viewModels()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        DaggerAppComponent.create().inject(this)
+
+        setUpRecyclerView()
+
+        observeLiveData()
     }
+
+    private fun setUpRecyclerView() {
+        recycler_view.apply {
+            setHasFixedSize(true)
+            itemAnimator = DefaultItemAnimator()
+            adapter = currencyAdapter
+        }
+    }
+
+    private fun observeLiveData() {
+        observeInProgress()
+        observeIsError()
+        observeCurrencies()
+    }
+
+    private fun observeInProgress() {
+        viewModel.repository.isInProgress.observe(this, Observer { isLoading ->
+            isLoading.let {
+                if (it) {
+                    error_text.visibility = View.GONE
+                    recycler_view.visibility = View.GONE
+                    fetch_progress.visibility = View.VISIBLE
+                } else {
+                    fetch_progress.visibility = View.GONE
+                }
+            }
+        })
+    }
+
+    private fun observeIsError() {
+        viewModel.repository.isError.observe(this, Observer { isError ->
+            isError.let {
+                if (it) {
+                    disableViewsOnError()
+                } else {
+                    error_text.visibility = View.GONE
+                    fetch_progress.visibility = View.VISIBLE
+                }
+            }
+        })
+    }
+
+    private fun disableViewsOnError() {
+        fetch_progress.visibility = View.VISIBLE
+        error_text.visibility = View.VISIBLE
+        recycler_view.visibility = View.GONE
+        currencyAdapter.setUpCurrencies(emptyList())
+        fetch_progress.visibility = View.GONE
+    }
+
+    private fun observeCurrencies() {
+        viewModel.repository.currencies.observe(this, Observer { currencies ->
+            currencies.let {
+                if (it != null && it.isNotEmpty()) {
+                    fetch_progress.visibility = View.VISIBLE
+                    recycler_view.visibility = View.VISIBLE
+                    currencyAdapter.setUpCurrencies(it)
+                    error_text.visibility = View.GONE
+                    fetch_progress.visibility = View.GONE
+                } else {
+                    disableViewsOnError()
+                }
+            }
+        })
+    }
+
 }
