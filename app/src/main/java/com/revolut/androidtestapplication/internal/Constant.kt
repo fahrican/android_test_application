e51a2e41f53c651b0ac6933834f1753070861b30
Fahri MacBook <fahri-can@web.de>
2020-07-07 23:08:12 +0200

    Create constant for EUR value.

--- app/src/main/java/com/revolut/androidtestapplication/internal/Constant.kt
+++ app/src/main/java/com/revolut/androidtestapplication/internal/Constant.kt
@@ -4,3 +4,4 @@ package com.revolut.androidtestapplication.internal
 const val BASE_URL = "https://hiring.revolut.codes/"
 const val ENDPOINT = "api/android/latest"
 const val BASE_CURRENCY = "base"
+const val EURO = "EUR"
