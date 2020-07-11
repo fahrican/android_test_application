396f88c44bb36b1576df3350a5c658bdb5648f0b
Fahri MacBook <fahri-can@web.de>
2020-07-11 17:34:44 +0200

    Add for now the user entered amount variable.

--- app/src/main/java/com/revolut/androidtestapplication/internal/Constant.kt
+++ app/src/main/java/com/revolut/androidtestapplication/internal/Constant.kt
@@ -6,3 +6,5 @@ const val ENDPOINT = "api/android/latest"
 const val BASE_CURRENCY = "base"
 const val EURO = "EUR"
 const val POSITION_OF_EURO_IN_DEFAULT_LIST = 8
+
+var userEnteredAmount: Double = 100.00 //todo: find a better place
