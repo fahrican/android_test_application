ae3bf500f1d3ca4d41c481b2af7244b45d77e566
Fahri MacBook <fahri-can@web.de>
2020-07-12 01:15:17 +0200

    Use two way data binding.

--- app/src/main/java/com/revolut/androidtestapplication/model/CurrencyItem.kt
+++ app/src/main/java/com/revolut/androidtestapplication/model/CurrencyItem.kt
@@ -1,15 +1,22 @@
 package com.revolut.androidtestapplication.model
 
-import com.revolut.androidtestapplication.internal.userEnteredAmount
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+import androidx.databinding.library.baseAdapters.BR
 
 data class CurrencyItem(
     var flag: String,
     var shortName: String,
     var fullName: String,
     var rate: Double
-) {
-    fun multiplyAmountWithRates(): String {
-        return (rate * userEnteredAmount).toString()
-    }
+) : BaseObservable() {
 
+    @Bindable
+    var rateTimesAmount: String = rate.toString()
+        set(amount) {
+            val amountAsDouble = amount.toDouble()
+            val number2digits:Double = String.format("%.2f", amountAsDouble).toDouble()
+            field = number2digits.toString()
+            notifyPropertyChanged(BR.rateTimesAmount)
+        }
 }
