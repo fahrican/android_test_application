8d54d3e0c951b3f2af7cd50a1b6b97b15723a63a
Fahri MacBook <fahri-can@web.de>
2020-07-11 17:37:02 +0200

    Modify data class to amount gets multiplied with current rate.

--- app/src/main/java/com/revolut/androidtestapplication/model/CurrencyItem.kt
+++ app/src/main/java/com/revolut/androidtestapplication/model/CurrencyItem.kt
@@ -1,9 +1,15 @@
 package com.revolut.androidtestapplication.model
 
+import com.revolut.androidtestapplication.internal.userEnteredAmount
+
 data class CurrencyItem(
     var flag: String,
     var shortName: String,
     var fullName: String,
-    var rate: Double,
-    var rateToString: String = rate.toString()
-)
+    var rate: Double
+) {
+    fun multiplyAmountWithRates(): String {
+        return (rate * userEnteredAmount).toString()
+    }
+
+}
