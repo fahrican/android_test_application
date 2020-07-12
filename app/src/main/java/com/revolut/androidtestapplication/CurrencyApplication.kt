94d2e6a78e9d176ff59e2b54cd87cfe85318e84c
Fahri MacBook <fahri-can@web.de>
2020-07-12 14:22:08 +0200

    Create application class to store user entered amount.

---
+++ app/src/main/java/com/revolut/androidtestapplication/CurrencyApplication.kt
@@ -0,0 +1,11 @@
+package com.revolut.androidtestapplication
+
+import android.app.Application
+
+class CurrencyApplication : Application() {
+
+    companion object {
+        var userEnteredAmount: Double = 1.00
+    }
+
+}
